6d029fb067bc74876ddcc72168857b4177020c07
JBeaddie <beaddiej@gmail.com>
2021-11-06 14:55:48 +0000

    Tested and fixed random function.

--- libc/rand.c
+++ libc/rand.c
@@ -1,10 +1,12 @@
-static x0 = 12;  // seed
-static a;        // multiplier
-static b;        // increment
-static m;        // modulus
+static int a = 9;       // multiplier
+static int b = 13;      // increment
+static int m = 17;      // modulus
 
 
 int generate_rand() {
-    static x0 = (a * x0 + b) % m;
+    // Set seed
+    static int x0 = 12;
+
+    x0 = (a * x0 + b) % m;
     return x0;
 }
