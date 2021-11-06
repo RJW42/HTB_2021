6d0f251d16ea9a4ca7c395c0475ddcccf89f88e6
fas20 <findlaysloan212@outlook.com>
2021-11-06 12:14:26 +0000

    reverted memset

--- libc/mem.c
+++ libc/mem.c
@@ -7,8 +7,8 @@ void memory_copy(char *source, char *dest, int nbytes) {
     }
 }
 
-void memory_set(u32 *dest, u8 val, u32 len) {
-    u32 *temp = (u32 *)dest;
+void memory_set(u8 *dest, u8 val, u32 len) {
+    u8 *temp = (u8 *)dest;
     for ( ; len != 0; len--) *temp++ = val;
 }
 
