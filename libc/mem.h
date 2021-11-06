cdfb99851150d5c31795aa747690ee9fcda38ea2
isaacwatson1207 <isaac.watson1207@gmail.com>
2021-11-06 12:07:39 +0000

    Added calloc as malloc+memset

--- libc/mem.h
+++ libc/mem.h
@@ -4,7 +4,8 @@
 #include "../cpu/types.h"
 
 void memory_copy(char *source, char *dest, int nbytes);
-void memory_set(u8 *dest, u8 val, u32 len);
+void memory_set(u32 *dest, u8 val, u32 len);
 u32 malloc(u32 size);
+u32 calloc(u32 size);
 
 #endif
